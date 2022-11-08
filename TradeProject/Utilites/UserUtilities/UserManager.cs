6fa227d22eea8e5f33050fef117170c5491add56
SDFTW-GIT <wagoner.jnj99@gmail.com>
2022-11-08 14:41:43 -0800

    Cleaning

--- TradeProject/Utilites/UserUtilities/UserManager.cs
+++ TradeProject/Utilites/UserUtilities/UserManager.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using TradeProject.DataModels;
+﻿using TradeProject.DataModels;
 
 namespace TradeProject.Utilites.UserUtilities
 {
--- TradeProject/Utilites/UserUtilities/UserVerifier.cs
+++ TradeProject/Utilites/UserUtilities/UserVerifier.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using TradeProject.DataModels;
+﻿using TradeProject.DataModels;
 
 namespace TradeProject.Utilites.UserUtilities
 {
